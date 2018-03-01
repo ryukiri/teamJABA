4389b758c7a7ca42492fab15cc87c711cf122af8
bonngyn <bon.ngyn@gmail.com>
2018-03-01 02:23:23 -0800

    simple launch screen text and image for now

--- eats/Swipeable-View-Stack/API/YelpRepo.swift
+++ eats/Swipeable-View-Stack/API/YelpRepo.swift
@@ -56,12 +56,6 @@ class YelpRepo {
         }
     }
     
-    private func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            completion(data, response, error)
-            }.resume()
-    }
-    
     public func getBusinesses() -> [BusinessCard] {
         return self.businesses
     }
