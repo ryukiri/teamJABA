2981cbd00ac3a2a56b5ca1b6acc415f48c00c987
Austin Quach <austinquachh@gmail.com>
2018-03-11 23:09:18 -0700

    Add feature: notification reminders

--- eats/Swipeable-View-Stack/View Controllers/ChoseViewController.swift
+++ eats/Swipeable-View-Stack/View Controllers/ChoseViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import EventKit
+import UserNotifications
 
 class ChoseViewController: UIViewController, GMSMapViewDelegate {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var mapView: GMSMapView!
+    var eventStore = EKEventStore()
+    var calendars:Array<EKCalendar> = []
     
     let yelpRepo = YelpRepo.shared
     
@@ -64,6 +68,21 @@ class ChoseViewController: UIViewController, GMSMapViewDelegate {
                 GoogleMapsHelper.createMarker(title: "Test", coords: businessCoords, mapView: self.mapView)
             }
         }
+        
+        // get permission
+        eventStore.requestAccess(to: EKEntityType.reminder, completion:
+            {(granted, error) in
+                if !granted {
+                    print("Access to store not granted")
+                }
+        })
+        
+        // you need calender's permission for the reminders as they live there
+        calendars = eventStore.calendars(for: EKEntityType.reminder)
+        
+        for calendar in calendars as [EKCalendar] {
+            print("Calendar = \(calendar.title)")
+        }
     }
 
     let regionRadius: CLLocationDistance = 500
@@ -110,6 +129,78 @@ class ChoseViewController: UIViewController, GMSMapViewDelegate {
         if UIApplication.shared.canOpenURL(URL(string:"sms:")!) {
             UIApplication.shared.open(URL(string:"sms:")!, options: [:], completionHandler: nil)
         }
+    }
+    
+    
+    @IBAction func remindButton(_ sender: Any) {
+        let name = business?.name
+        
+        let center = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .sound]
+        center.requestAuthorization(options: options) { (granted, error) in
+            if !granted {
+                print("Something went wrong")
+            }
+        }
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus != .authorized {
+                // Notifications not allowed
+            }
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Don't forget about"
+        content.body = "your food plans at \(name!)"
+        content.sound = UNNotificationSound.default()
+        
+        let alert = UIAlertController(title: "Reminder", message: "When do you want us to remind you?", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "1 min", style: UIAlertActionStyle.default, handler: {
+            (act: UIAlertAction) in
+            print("1 min")
+  
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: false)
+            let identifier = "UYLLocalNotification"
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+            
+            center.add(request, withCompletionHandler: { (error) in
+                if let error = error {
+                    print("Something went wrong.")
+                }
+            })
+
+        }))
+        alert.addAction(UIAlertAction(title: "1 hour", style: UIAlertActionStyle.default, handler: {
+            (act: UIAlertAction) in
+            print("1 hour")
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3600, repeats: false)
+            let identifier = "UYLLocalNotification"
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+            
+            center.add(request, withCompletionHandler: { (error) in
+                if let error = error {
+                    print("Something went wrong.")
+                }
+            })
+            
+        }))
+        alert.addAction(UIAlertAction(title: "6 hours", style: UIAlertActionStyle.default, handler: {
+            (act: UIAlertAction) in
+            print("1 hour")
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 21600, repeats: false)
+            let identifier = "UYLLocalNotification"
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+            
+            center.add(request, withCompletionHandler: { (error) in
+                if let error = error {
+                    print("Something went wrong.")
+                }
+            })
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
